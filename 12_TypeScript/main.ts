67daf223d80cc6ef201583b162dc376726c9d9ad
Miklós Tóth (Mike) <tothmiklostibor@gmail.com>
2024-12-12 18:45:42 +0100

    Day 12, part 1

---
+++ 12_TypeScript/main.ts
@@ -0,0 +1,119 @@
+
+// const inputPath = "input_sample.txt"
+const inputPath = "input.txt"
+
+async function main(): Promise<void> {
+  const data = await loadFile(inputPath)
+  const plots = convert(data)
+  const regions = divideRegions(plots)
+  const allCosts = regions
+      .reduce((acc, item) => acc + item.cost, 0)
+  console.log(allCosts)
+}
+
+class Plot {
+  public neighbours: Plot[] = []
+  public name: string
+  constructor(name: string) {
+    this.name = name
+  }
+
+  get fence(): number {
+    return 4 - this.neighbours
+        .filter(it => it.name === this.name)
+        .length
+  }
+}
+
+function convert(data: string[][]): Plot[] {
+  const map: Plot[][] = []
+  data.forEach((row, y) => {
+    const mapRow: Plot[] = []
+    map.push(mapRow)
+
+    row.forEach((cell, x) => {
+      const plot = new Plot(cell)
+      if (y > 0) {
+        const neighbour = map[y-1][x]
+        plot.neighbours.push(neighbour)
+        neighbour.neighbours.push(plot)
+      }
+      if (x > 0) {
+        const neighbour = map[y][x-1]
+        plot.neighbours.push(neighbour)
+        neighbour.neighbours.push(plot)
+      }
+      mapRow.push(plot)
+    })
+  })
+
+  return map.flat()
+}
+
+class Region {
+  public plots: Plot[] = []
+  public name: string
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  get fence() {
+    return this.plots
+        .reduce((acc, it) => acc + it.fence, 0)
+  }
+  get area() {
+    return this.plots.length
+  }
+  get cost() {
+    return this.fence * this.area
+  }
+}
+
+function divideRegions(plots: Plot[]): Region[] {
+  const regions: Region[] = []
+  const checkedPlots: Plot[] = []
+
+  plots.forEach((plot) => {
+    if (checkedPlots.includes(plot)) {
+      return
+    }
+
+    const region = new Region(plot.name)
+    checkPlot(plot, region, checkedPlots)
+    regions.push(region)
+  })
+
+  return regions
+}
+
+function checkPlot(plot: Plot, region: Region, checkedPlots: Plot[]): void {
+  if (plot.name != region.name) {
+    return
+  }
+
+  if (checkedPlots.includes(plot)) {
+    return
+  }
+
+  region.plots.push(plot)
+  checkedPlots.push(plot)
+
+  plot.neighbours.forEach((neighbour) => {
+    checkPlot(neighbour, region, checkedPlots)
+  })
+}
+
+async function loadFile(path: string): Promise<string[][]> {
+  const decoder = new TextDecoder("utf-8");
+  const data = await Deno.readFile(path);
+  return decoder
+      .decode(data)
+      .split(/\n/)
+      .filter(l => l !== "")
+      .map((line) => line.split(""));
+}
+
+
+if (import.meta.main) {
+  await main()
+}
