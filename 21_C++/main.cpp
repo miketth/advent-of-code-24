5cdede0426673cb186ddc7bb30fd13b3a3de7e9e
Miklós Tóth (Mike) <tothmiklostibor@gmail.com>
2024-12-23 00:10:34 +0100

    Day 21, part 2

--- 21_C++/main.cpp
+++ 21_C++/main.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 
 using std::string;
+using std::string_view;
 using std::vector;
 using std::shared_ptr;
 
@@ -46,49 +47,13 @@ const std::vector<std::vector<char>> dirpad = {
         }
     }
 
-    throw std::runtime_error("key not found");
+    throw std::runtime_error("keypad key not found");
 }
 
-class CacheKey {
-    bool is_combo = false;
-    std::pair<char, char> simple = { '\0', '\0' };
-    string combo;
+typedef std::pair<char, char> KeypadCacheKey;
 
-public:
-    CacheKey(const string& combo) {
-        this->is_combo = true;
-        this->combo = combo;
-    }
-
-    CacheKey(const std::pair<char, char>& simple) {
-        this->is_combo = false;
-        this->simple = simple;
-    }
-
-    auto operator<=>(const CacheKey& rhs) const {
-        if (is_combo != rhs.is_combo) {
-            return std::strong_ordering::less;
-        }
-        if (is_combo) {
-            return combo <=> rhs.combo;
-        }
-        return simple <=> rhs.simple;
-    }
-
-    auto operator==(const CacheKey& rhs) const {
-        if (is_combo != rhs.is_combo) {
-            return false;
-        }
-        if (is_combo) {
-            return combo == rhs.combo;
-        }
-        return simple == rhs.simple;
-    }
-};
-
-string best_path(const char& from, const char& to, std::map<CacheKey, string>& cache, const bool is_numpad = false) {
-    const std::pair simple_cache_key = { from, to };
-    const CacheKey cache_key = simple_cache_key;
+string_view best_path(const char& from, const char& to, std::map<KeypadCacheKey, string>& cache, const bool is_numpad = false) {
+    const KeypadCacheKey cache_key = { from, to };
     if (cache.contains(cache_key)) {
         return cache[cache_key];
     }
@@ -120,46 +85,90 @@ string best_path(const char& from, const char& to, std::map<CacheKey, string>& c
 
 
     if (x_diff == 0 || y_diff == 0) {
-        auto solution = horizontal + vertical;
-        cache[cache_key] = solution ;
-        return solution;
+        cache[cache_key] = horizontal + vertical;
+        return cache[cache_key];
     }
 
     // if horizontal->vertical isn't possible
     if (keypad[from_y][to_x] == '\0') {
-        auto solution = vertical + horizontal;
-        cache[cache_key] = solution;
-        return solution;
+        cache[cache_key] = vertical + horizontal;
+        return cache[cache_key];
     }
 
     // if vertical->horizontal isn't possible
     if (keypad[to_y][from_x] == '\0') {
-        auto solution = horizontal + vertical;
-        cache[cache_key] = solution;
-        return solution;
+        cache[cache_key] = horizontal + vertical;
+        return cache[cache_key];
     }
 
     // if left: horiz->vert is better
     if (x_diff < 0) {
-        auto solution = horizontal + vertical;
-        cache[cache_key] = solution;
-        return solution;
+        cache[cache_key] = horizontal + vertical;
+        return cache[cache_key];
     }
 
     // if right: vert->horiz is better
-    auto solution = vertical + horizontal;
-    cache[cache_key] = solution;
-    return solution;
+    cache[cache_key] = vertical + horizontal;
+    return cache[cache_key];
 }
 
-string best_path(const string& combination, std::map<CacheKey, string>& cache, const bool is_numpad = false) {
+string best_path(const string& combination, std::map<KeypadCacheKey, string>& cache, const bool is_numpad = false) {
     string ret;
     char prev = 'A';
     for (const auto& next: combination) {
         auto path_here = best_path(prev, next, cache, is_numpad);
-        ret += path_here + 'A';
+        ret += path_here;
+        ret += 'A';
         prev = next;
     }
+
+    return ret;
+}
+
+vector<char> states_lower_or_equal_depth(vector<char>& states, const int depth) {
+    return {states.begin()+depth, states.end()};
+}
+
+// depth, lower states, combination
+typedef std::tuple<int, vector<char>, string> SimCacheKey;
+// saved states, return
+typedef std::tuple<vector<char>, unsigned long long int> SimCacheVal;
+
+unsigned long long int simulate_dirpad(
+    const string_view& combination,
+    std::map<SimCacheKey, SimCacheVal>& cache,
+    std::map<KeypadCacheKey, string>& keypad_cache,
+    vector<char>& states,
+    const int max_depth, const int depth = 1
+) {
+    const auto cache_key = SimCacheKey(depth, states_lower_or_equal_depth(states, depth), combination);
+    if (cache.contains(cache_key)) {
+        const auto& [cached_states, len] = cache[cache_key];
+        for (int i = 0; i < cached_states.size(); i++) {
+            states[i+depth] = cached_states[i];
+        }
+        return len;
+    }
+
+    unsigned long long int ret = 0;
+
+    char& prev = states[depth];
+
+    for (const auto& next: combination) {
+        auto path_here = best_path(prev, next, keypad_cache, false);
+        if (depth == max_depth) {
+            ret += path_here.length();
+            ret++; // for the 'A'
+        } else {
+            const auto one_layer_down = simulate_dirpad(path_here, cache, keypad_cache, states, max_depth, depth+1);
+            ret += one_layer_down;
+            const auto one_layer_down_a = simulate_dirpad("A", cache, keypad_cache, states, max_depth, depth+1);
+            ret += one_layer_down_a;
+        }
+        prev = next;
+    }
+
+    cache[cache_key] = SimCacheVal(states_lower_or_equal_depth(states, depth), ret);
 
     return ret;
 }
@@ -173,17 +182,21 @@ int main() {
 
     unsigned long int sum = 0;
 
-    std::map<CacheKey, string> numpad_cache;
-    std::map<CacheKey, string> dirpad_cache;
+    std::map<KeypadCacheKey, string> numpad_cache;
+    std::map<KeypadCacheKey, string> dirpad_cache;
+    std::map<SimCacheKey, SimCacheVal> sim_cache;
     for (const auto& code: codes) {
+        std::cout << "solving code " << code << std::endl;
+
         vector<string> final_paths;
 
         auto path = best_path(code, numpad_cache, true);
-        for (int i = 0; i < 2; i++) {
-            path = best_path(path, dirpad_cache);
-        }
 
-        sum += path.length() * stoi(code);
+        constexpr int depth = 25;
+        auto states = vector(depth+1, 'A');
+        const auto len = simulate_dirpad(path, sim_cache, dirpad_cache, states, depth);
+
+        sum += len * stoi(code);
     }
 
     std::cout << sum << std::endl;
