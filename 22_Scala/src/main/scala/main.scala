936f18171192a90cdb36702414e7c442b3724aa2
Miklós Tóth (Mike) <tothmiklostibor@gmail.com>
2024-12-22 20:19:43 +0100

    Day 22, part 2

--- 22_Scala/src/main/scala/main.scala
+++ 22_Scala/src/main/scala/main.scala
@@ -8,10 +8,95 @@ val filename = "input.txt"
 def main(): Unit =
   val nums = readFile(filename)
 
-  val finalSecrets = nums
-    .map(n => nthSecretNum(n, 2000))
+  val finalSecrets = nums.map(n => nthSecretNum(n, 2000))
   val sum = finalSecrets.sum()
   println(sum)
+
+  val prices = nums.map(n => pricesForNIter(n, 2000))
+  val diffs = prices.map(p => calcDiffs(p));
+
+  val value = bruteForce(prices, diffs)
+
+  println(value)
+
+def bruteForce(prices: List[List[Int]], diffs: List[List[Int]]): Int =
+  val possible = -9 to 9
+  val matchingRanges = (for (d1 <- possible) yield {
+    println(s"Trying ${d1}")
+    val rangesStartingWith1 = diffs.map(d => d.zipWithIndex.dropRight(3).filter((v, idx) => v == d1).map((v, idx) => idx))
+    for (d2 <- possible) yield {
+      val rangesStartingWith12 = diffs.zip(rangesStartingWith1).map((d, ranges) => {
+        val goodRanges = ranges.filter(idx => d(idx+1) == d2)
+        (d, goodRanges)
+      })
+      for (d3 <- possible) yield {
+        val rangesStartingWith123 = rangesStartingWith12.map((d, ranges) => {
+          val goodRanges = ranges.filter(idx => d(idx+2) == d3)
+          (d, goodRanges)
+        })
+        for (d4 <- possible) yield {
+          val indexesMatching1234 = rangesStartingWith123.map((d, ranges) => {
+            val goodRanges = ranges.filter(idx => d(idx+3) == d4)
+            if (goodRanges.isEmpty) {
+              -1
+            } else {
+              goodRanges.head
+            }
+          })
+
+          val pricePerVendor = prices
+            .zip(indexesMatching1234)
+            .filter((p, idx) => idx != -1)
+            .map((p, idx) => p(idx+4))
+          pricePerVendor.sum
+        }
+      }
+    }.flatten
+  }.flatten).flatten
+  matchingRanges.max
+
+
+def generateAllDiffs(): List[List[Int]] =
+  val possible = -9 to 9
+  (for (d1 <- possible)
+    yield for (d2 <- possible)
+      yield for (d3 <- possible)
+        yield for (d4 <- possible)
+          yield List(d1, d2, d3, d4)).flatten.flatten.flatten.toList
+
+def checkSolution(prices: List[List[Int]], diffs: List[List[Int]], solution: List[Int]): Int =
+  val indexes = diffs.map(d => d.indexOfSlice(solution))
+  prices.zip(indexes).map((p, idx) => getWithIndexOrZero(p, idx, solution.length)).sum()
+
+def getWithIndexOrZero(p: List[Int], idx: Int, offset: Int): Int =
+  if (idx < 0) {
+    return 0
+  }
+
+  p(idx+offset)
+
+def pricesForNIter(start: BigInt, n: Int): List[Int] =
+  val emptyList = List[Int]()
+  val rev = pricesForNIterHelper(start, n, emptyList)
+  rev.reverse
+
+@tailrec
+def pricesForNIterHelper(start: BigInt, n: Int, ret: List[Int]): List[Int] =
+  if (n == 0) {
+    return ret
+  }
+
+  val digit = start % 10
+  val next = ret.prepended(digit.toInt)
+
+  val current = nextSecretNum(start)
+  pricesForNIterHelper(current, n-1, next)
+
+
+def calcDiffs(prices: List[Int]): List[Int] =
+  val pricesShifted = prices.prepended(0)
+  pricesShifted.zip(prices).map((a, b) => b - a).drop(1)
+
 
 @tailrec
 def nthSecretNum(start: BigInt, n: Int): BigInt =
